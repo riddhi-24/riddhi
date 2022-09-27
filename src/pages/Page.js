eca71d53507c477dfc73eb0d08cdb39d9e4926b6
Riddhi patel <riddhi.sherasiya@marutitech.com>
2022-09-27 15:57:00 +0530

    Dynamic route single page app

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Page = () => {
-  return <div></div>;
-};
+  
+  let location = useLocation();
+
+  return (
+  <div id="me" style={{background : 'skyblue', height: '20px'}}>{`current URL : ${location.pathname || ""}`}</div>)
+}
 
 export default Page;
