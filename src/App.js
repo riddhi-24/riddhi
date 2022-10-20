4ca1c2fb1e8f1ccec7210acce9a07d2b6e3f45ec
riddhi <riddhi.sherasiya@marutitech.com>
2022-10-20 14:38:06 +0530

    removed unsed import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Page from "./pages/Page";
-import Error from "./pages/Error";
 
 export default function App() {
   return (
