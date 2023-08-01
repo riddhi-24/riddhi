035cdb76eea4b86a48add83408da7cc59795aedb
Riddhi <riddhi.sherasiya@marutitech.com>
2023-08-01 15:22:53 +0530

    web one

--- src/App.js
+++ src/App.js
@@ -1,37 +1,5 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import Page from "./pages/Page";
-import Error from "./pages/Error";
 
 export default function App() {
-  return (
-    <Router>
-      <Switch>
-     
-
-        <Route path="/sell">
-          <Page url={"buy"} />
-        </Route>
-
-
-        <Route path="/buy">
-          <Page url={"purchase"} />
-        </Route>
-
-
-        <Route path="/purchase">
-          <Page url={"product"} />
-        </Route>
-
-
-        <Route path="/product">
-          <Page url={"sell"} />
-        </Route>
-
-        <Route path="*" exact>
-          <Error />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  return (<div>web one</div>);
 }
