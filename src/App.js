24ef89c6693f3631dc425f895192f964657ecfc6
Riddhi <riddhi.sherasiya@marutitech.com>
2023-10-06 11:02:04 +0530

    made div centered

--- src/App.js
+++ src/App.js
@@ -2,5 +2,5 @@ import React from "react";
 import './App.css'
 
 export default function App() {
-  return (<div style={{'margin': '150px auto', 'width': '50%'}}><iframe width="640" height="480" src="https://embed.eu.wotnot.io/35brpYXEKg6z130417646268e6XgHmhe/bot/6QHPbryztqgs130553424674MufrOyG3?display_header=false&history_retention=false" frameborder="0"></iframe> </div>);
+  return (<div style={{'position' : 'absolute', 'transform' : 'translate(-50%, -50%)', 'top' : '50%', 'left' : '50%'}}><iframe width="1000" height="800" src="https://embed.eu.wotnot.io/35brpYXEKg6z130417646268e6XgHmhe/bot/6QHPbryztqgs130553424674MufrOyG3?display_header=false&history_retention=false" frameborder="0"></iframe> </div>);
 }
