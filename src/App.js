23f0536cd09292fa01ffc0faa1b68b1441fa3690
Riddhi <riddhi.sherasiya@marutitech.com>
2023-10-06 10:05:42 +0530

    removed warning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React, { Fragment } from "react";
+import React from "react";
 import './App.css'
-import {BrowserRouter, Switch, Link, Route} from 'react-router-dom'
-import Page from "./page";
 
 export default function App() {
   return (<div style={{'margin': '150px auto', 'width': '50%'}}><iframe width="640" height="480" src="https://embed.eu.wotnot.io/35brpYXEKg6z130417646268e6XgHmhe/bot/6QHPbryztqgs130553424674MufrOyG3?display_header=false&history_retention=false" frameborder="0"></iframe> </div>);
