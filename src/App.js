94f1ea5b96bb0026b82bccabbc059a9d68f2e54e
Riddhi <riddhi.sherasiya@wotnot.io>
2024-09-05 19:23:38 +0530

    added 'remove script'

--- src/App.js
+++ src/App.js
@@ -80,9 +80,15 @@ export default function App() {
 
   }
 
+  const onRemoveScript = () => {
+    removeElementsOnUnmount();
+  }
+
   return (<div>
-    <h1>This is my website</h1>``
+    <h1>This is my website</h1>
 <button onClick={e => onAddScript(e)}>Add script</button>
+<button onClick={e => onRemoveScript(e)}>remove script</button>
+
     {/* <iframe width="640" height="480" src="https://embed.dev.wotnot.io/ZLSS34AV63QR135844052724LKMotLtn/bot/5rKg3D27nsQk1147369188978bCdBQGo?display_header=false&history_retention=false&gtm_id=GTM-PS7CS8G7" frameborder="0"></iframe> */}
     
     {/* <button onClick={handleClick}>Click me</button> */}
