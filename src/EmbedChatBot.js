d6b9f63c115dd965f95ffe13646b0f23fa15b4b4
Riddhi <riddhi.sherasiya@wotnot.io>
2024-09-24 12:27:30 +0530

    Added embed via ifrmae & div

--- src/EmbedChatBot.js
+++ src/EmbedChatBot.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function EmbedBot() {
-  return (<div>
-    <h4>This is Embed version of chatbot</h4>
+  const typeMap = {'iframe' : 'IFRAME', 'div' : 'DIV'};
+  const [embedType, setEmbedType] = useState('iframe');
 
-    <iframe width="640" height="480" src="https://embed.wotnot.io/pt5ccHzjEgpw082454069938MsceKjSD/bot/54yeBwMzgAdy055455017858UO6PjXPd?display_header=false&history_retention=false" frameborder="0"></iframe>
+  const handleEmbedTypeChange = () => {
+    if (embedType === 'iframe') {
+      setEmbedType('div');
+      const scriptTag = document.createElement('script');
+      scriptTag.setAttribute('src', 'https://app.wotnot.io/chat-widget/pt5ccHzjEgpw082454069938MsceKjSD.js');
+      scriptTag.setAttribute('async', true);
+      scriptTag.setAttribute('widget-container-id', 'rootContainer');
+      scriptTag.setAttribute('widget-height', '500px');
+      scriptTag.setAttribute('widget-width', '600px');
+      scriptTag.setAttribute('widget-header-visible', false);
+      document.head.appendChild(scriptTag);
+    }
+    else {
+      setEmbedType('iframe');
+    }
+  }
 
-  </div>)
+  return (
+    <div>
+      <h4>{`Chatbot is embedded via ${typeMap[embedType]} element`}</h4>
+      <button className='button' onClick={e => handleEmbedTypeChange()}>Change embedType</button>
+      <h1></h1>
+      {embedType === 'iframe' ? <iframe id='embedIframe' width="640" height="480" src="https://embed.wotnot.io/pt5ccHzjEgpw082454069938MsceKjSD/bot/54yeBwMzgAdy055455017858UO6PjXPd?display_header=false&history_retention=false" frameBorder="0"></iframe> :
+        <div id="rootContainer"></div>}
+
+    </div>)
 }
