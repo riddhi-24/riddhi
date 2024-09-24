0bb4b6cacfd92e1c6f0e8098421b309cd3a98656
Riddhi <riddhi.sherasiya@wotnot.io>
2024-09-24 13:08:23 +0530

    added BG

--- src/EmbedChatBot.js
+++ src/EmbedChatBot.js
@@ -11,7 +11,7 @@ export default function EmbedBot() {
       scriptTag.setAttribute('src', 'https://app.wotnot.io/chat-widget/pt5ccHzjEgpw082454069938MsceKjSD.js');
       scriptTag.setAttribute('async', true);
       scriptTag.setAttribute('widget-container-id', 'rootContainer');
-      scriptTag.setAttribute('widget-height', '500px');
+      scriptTag.setAttribute('widget-height', '400px');
       scriptTag.setAttribute('widget-width', '600px');
       scriptTag.setAttribute('widget-header-visible', false);
       document.head.appendChild(scriptTag);
@@ -27,7 +27,7 @@ export default function EmbedBot() {
       <button className='button' onClick={e => handleEmbedTypeChange()}>Change embedType</button>
       <h1></h1>
       {embedType === 'iframe' ? <iframe id='embedIframe' width="640" height="480" src="https://embed.wotnot.io/pt5ccHzjEgpw082454069938MsceKjSD/bot/54yeBwMzgAdy055455017858UO6PjXPd?display_header=false&history_retention=false" frameBorder="0"></iframe> :
-        <div id="rootContainer"></div>}
+        <div id="rootContainer" style={{background : 'white'}}></div>}
 
     </div>)
 }
